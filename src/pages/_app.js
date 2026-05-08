dc24078b628679c6523d7cd1f4945f6e5f174867
Dev Lewa <omolewastephen@gmail.com>
2026-05-09 00:31:38 +0100

    fix: disable React Query Devtools in production

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -64,7 +64,7 @@ export default function App({ Component, pageProps }) {
         <AppWrapper>
           <Component {...pageProps} />
         </AppWrapper>
-        <ReactQueryDevtools initialIsOpen={false} />
+        {process.env.NODE_ENV !== 'production' && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
     </MainProvider>
   );
